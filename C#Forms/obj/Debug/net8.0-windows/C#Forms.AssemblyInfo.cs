b2b8b46d6adbc11e25bb5c63a4b7d62900d3e82b
ShadrinaM <143996514+ShadrinaM@users.noreply.github.com>
2024-11-14 23:45:44 +0300

    лабы 3_2, 4, 5_2

--- C#Forms/obj/Debug/net8.0-windows/C#Forms.AssemblyInfo.cs
+++ C#Forms/obj/Debug/net8.0-windows/C#Forms.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#Forms")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ba16e44aef1d1c8ab6f56e07a10959b358248be")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#Forms")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#Forms")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
