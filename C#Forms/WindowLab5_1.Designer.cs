c8cca8e9bc45b09802530bba83252b32afc977d9
BusinaLapusina <busina_lapusina@mail.ru>
2024-12-11 01:22:39 +0300

    интерполяция сплайнами

--- C#Forms/WindowLab5_1.Designer.cs
+++ C#Forms/WindowLab5_1.Designer.cs
@@ -28,18 +28,162 @@
         /// </summary>
         private void InitializeComponent()
         {
+            plotView1 = new OxyPlot.WindowsForms.PlotView();
+            numericUpDown1 = new NumericUpDown();
+            label1 = new Label();
+            label2 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            label3 = new Label();
+            label4 = new Label();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
             SuspendLayout();
+            // 
+            // plotView1
+            // 
+            plotView1.Location = new Point(12, 12);
+            plotView1.Name = "plotView1";
+            plotView1.PanCursor = Cursors.Hand;
+            plotView1.Size = new Size(537, 537);
+            plotView1.TabIndex = 0;
+            plotView1.Text = "plotView1";
+            plotView1.ZoomHorizontalCursor = Cursors.SizeWE;
+            plotView1.ZoomRectangleCursor = Cursors.SizeNWSE;
+            plotView1.ZoomVerticalCursor = Cursors.SizeNS;
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(709, 147);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(120, 23);
+            numericUpDown1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Semibold", 14F, FontStyle.Bold);
+            label1.Location = new Point(555, 36);
+            label1.Name = "label1";
+            label1.Size = new Size(98, 25);
+            label1.TabIndex = 2;
+            label1.Text = "Задание 1";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
+            label2.Location = new Point(555, 149);
+            label2.Name = "label2";
+            label2.Size = new Size(109, 21);
+            label2.TabIndex = 3;
+            label2.Text = "Число узлов:";
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 12F);
+            button1.Location = new Point(555, 183);
+            button1.Name = "button1";
+            button1.Size = new Size(417, 52);
+            button1.TabIndex = 4;
+            button1.Text = "Полиномиальная интерполяция";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 12F);
+            button2.Location = new Point(555, 241);
+            button2.Name = "button2";
+            button2.Size = new Size(417, 52);
+            button2.TabIndex = 5;
+            button2.Text = "Интерполяция кубическими сплайнами";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Segoe UI", 12F);
+            button3.Location = new Point(555, 497);
+            button3.Name = "button3";
+            button3.Size = new Size(417, 52);
+            button3.TabIndex = 6;
+            button3.Text = "Построить кубический сплайн для функции, заданной таблицей";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
+            label3.Location = new Point(659, 78);
+            label3.Name = "label3";
+            label3.Size = new Size(123, 21);
+            label3.TabIndex = 7;
+            label3.Text = "y(x)=1/1+25x^2";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI Semibold", 14F, FontStyle.Bold);
+            label4.Location = new Point(555, 335);
+            label4.Name = "label4";
+            label4.Size = new Size(101, 25);
+            label4.TabIndex = 8;
+            label4.Text = "Задание 2";
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.CellBorderStyle = TableLayoutPanelCellBorderStyle.Single;
+            tableLayoutPanel1.ColumnCount = 5;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+            tableLayoutPanel1.Location = new Point(659, 390);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 2;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Size = new Size(207, 76);
+            tableLayoutPanel1.TabIndex = 9;
             // 
             // WindowLab5_1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1125, 748);
+            ClientSize = new Size(984, 561);
+            Controls.Add(tableLayoutPanel1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(numericUpDown1);
+            Controls.Add(plotView1);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "WindowLab5_1";
             Text = "WindowLab5_1";
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private OxyPlot.WindowsForms.PlotView plotView1;
+        private NumericUpDown numericUpDown1;
+        private Label label1;
+        private Label label2;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Label label3;
+        private Label label4;
+        private TableLayoutPanel tableLayoutPanel1;
     }
 }
